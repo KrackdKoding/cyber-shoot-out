b7da2af21a639ee2f3fd035db9781413f20d8996
jwcallen <2241983+jwcallen@users.noreply.github.com>
2018-12-14 15:01:10 -0500

    Updated score.py
    Created Main2() method for when the arduino -> pi communication did not work properly

--- html/score.py
+++ html/score.py
@@ -27,16 +27,50 @@ def Main():
     # Forever loop
     while True:
 
-        name = raw_input("Enter your name: ")
-        name = ''.join(c for c in name if c in onlyWhiteSpaceAlphaNum)
+        inName = raw_input("Enter your name: ")
+        inName = ''.join(c for c in name if c in onlyWhiteSpaceAlphaNum)
 
         score = ''
         # TODO Get score from arduino
         while not len(score):
             score = ser.readline()
 
-        conn.execute("INSERT INTO scores (name, score, device) VALUES ({},{},{})".format(name, score, args.arduinoName))
+        conn.execute("INSERT INTO scores (name, score, device) VALUES ({},{},{})".format(inName, score, args.arduinoName))
         conn.commit()
 
+def Main2():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-an", "--arduinoName", required=True, help="name of the arduino (expected a1 or a2)")
+    
+    args = parser.parse_args()
+    
+    try:
+        conn = sqlite3.connect(sqlDB)
+    except Exception as e:
+        print e
+        sys.exit(1)
+        
+    while True:
+        inName = raw_input("Enter your name: ")
+        inName = ''.join(c for c in inName if c in onlyWhiteSpaceAlphaNum)
+        while True:
+            score = raw_input("Enter the Score: ")
+            score = ''.join(c for c in score if c in (string.digits + '-'))
+            if len(score) > 1 and score[0] == '-':
+                score = score[0] + ''.join(c for c in score[1:] if c in string.digits)
+            else:
+                score = ''.join(c for c in score if c in string.digits)
+            if len(score) > 0:
+                if int(score) > 7:
+                    score = 7
+                elif int(score) < -7:
+                    score = -7
+                break
+                
+        conn.execute("INSERT INTO scores (name, score, device) VALUES ({},{},{})".format(inName, score, args.arduinoName))
+        conn.commit()
+        
+        
 if __name__ == '__main__':
-    Main()
+    # Main()
+    Main2()
